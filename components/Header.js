449a307eff1c3ad3ea2d93183627a87f9dc8d2ca
csmurillo <csmurillo00@gmail.com>
2022-03-05 19:29:53 -0800

    fixed header

--- components/Header.js
+++ components/Header.js
@@ -1,73 +1,140 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components';
 const Header = () =>{
+
+    const [menuActive, setMenuActive]= useState(false);
+
+    const menuClick = ()=>{
+        setMenuActive(!menuActive);
+    };
+
     return (
-        <HeaderContainer className="spacebetween">
-            <LogoContainer>
+        <HeaderContainer>
+            <LogoContainer className='center'>
                 <Logo src="/images/logo.svg"></Logo>
             </LogoContainer>
-            <HamburgerIconContainer>
-                <HamburgerIcon src="/images/icon-hamburger.svg"></HamburgerIcon>
-            </HamburgerIconContainer>
-            <DesktopLinksContainer>
-                <DesktopLinkGroup>
-                    <Links className='center'>About</Links>
-                    <Links className='center'>Services</Links>
-                    <Links className='center'>Projects</Links>
+            <HamburgerContainer className='center'>
+                <HamburgerIcon src="/images/icon-hamburger.svg" onClick={menuClick}></HamburgerIcon>
+            </HamburgerContainer>
+            <DesktopNavLinks>
+                <DesktopLinks className='center'><p>About</p></DesktopLinks>
+                <DesktopLinks className='center'><p>Services</p></DesktopLinks>
+                <DesktopLinks className='center'><p>Projects</p></DesktopLinks>
+                <DesktopLinks>
                     <ContactButton>CONTACT</ContactButton>
-                </DesktopLinkGroup>
-            </DesktopLinksContainer>
+                </DesktopLinks>
+            </DesktopNavLinks>
+            {menuActive &&
+                <MobileNavLinksContainer className='center'>
+                    <MobileNavLinks>
+                        <MobileNavLinksTriangle></MobileNavLinksTriangle>
+                        <MobileNavBackground>
+                            <MobileLinks className='center'><p>About</p></MobileLinks>
+                            <MobileLinks className='center'><p>Services</p></MobileLinks>
+                            <MobileLinks className='center pad-bottom-1'><p>Projects</p></MobileLinks>
+                            <MobileLinks className='center'>
+                                <ContactButton color={'hsl(51, 100%, 49%)'}>CONTACT</ContactButton>
+                            </MobileLinks>
+                        </MobileNavBackground>
+                    </MobileNavLinks>
+                </MobileNavLinksContainer>
+            }
         </HeaderContainer>
     );
-};
-const HeaderContainer = styled.div`
-    width:100%;
-`;
-const LogoContainer = styled.div`
-    padding:30px 0px 0px 60px;
+}
+const HeaderContainer = styled.header`
+    padding:40px 30px 0px 30px;
+    position:relative;
     display:flex;
-    margin:0px;
-    @media (max-width: 768px){
-        padding:40px 0px 0px 30px;
-    }
+    justify-content:space-between;
+`;
+const LogoContainer= styled.div`
+
 `;
 const Logo = styled.img`
-    height:40px;
-    @media (max-width: 768px){
-        height:24px;
-    }
+
 `;
-const HamburgerIconContainer = styled.div`
-    padding:40px 30px 0px 0px;
-`;
-const HamburgerIcon = styled.img`
-    width:32px; height:24px;
+const HamburgerContainer=styled.div`
     @media (min-width: 768px){
-        display:none;
+        display:none!important;
+    }
+`
+const HamburgerIcon=styled.img`
+    &:hover{
+        cursor:pointer;
     }
 `;
-const DesktopLinksContainer = styled.div`
-    padding:30px 60px 0px 0px;
+const DesktopNavLinks= styled.nav`
+    display:flex;
     @media (max-width: 768px){
         display:none!important;
     }
 `;
-const DesktopLinkGroup = styled.div`
-    display:flex;
-`;
-const Links = styled.div`
+const DesktopLinks=styled.div`
+    padding:0px 20px 0px 20px;
     color:white;
-    padding:20px;
-    margin-right:50px !important;
-    &:hover {
+    & p {
+        margin:0;
+        padding:0;
+        &:hover{
+            cursor:pointer;
+        }
+    }
+`;
+const ContactButton=styled.button`
+    background-color:${props=>props.color};
+    font-family: 'Fraunces',serif;
+    color: black;
+    border-radius: 40%/100%;
+    border: none;
+    padding: 18px 30px;
+    &:hover{
         cursor:pointer;
     }
 `;
-const ContactButton = styled.button`
-    font-family: 'Fraunces', serif;
-    padding:20px;
-    background-color:white;
-    border-radius:35%/60%;
-    border:none;
+const MobileNavLinksContainer=styled.div`
+    position:absolute;
+    left:0; bottom:0;
+    top:190%;
+    width:100%;
+    height:100px;
+    @media (min-width: 768px){
+        display:none!important;
+    }
 `;
+const MobileNavLinksTriangle = styled.div`
+    position: absolute;
+    top:-30%;
+    right: 30px;
+    width: 0; 
+    height: 0; 
+    border-top: 30px solid transparent;
+    border-bottom: 30px solid transparent; 
+    border-right:30px solid white; 
+`;
+const MobileNavLinks=styled.div`
+    position:relative;
+    width:100%; 
+    height:100%;
+    padding: 0px 30px 0px 30px;
+`;
+const MobileNavBackground =styled.div`
+    background-color:white;
+    padding:30px;
+`;
+const MobileLinks=styled.div`
+    background-color:white;
+    padding: 20px 0px 20px 0px;
+    & p {
+        color:hsl(167, 40%, 24%);
+        font-weight:600;
+        color:hsl(167, 40%, 24%);
+        padding: 0;
+        margin: 0;
+        &:hover{
+            cursor:pointer;
+        }
+    }
+`;
+
 export default Header;

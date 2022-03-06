778cbec1f55561e0f6ad7d80ee954ba0a8dade85
csmurillo <csmurillo00@gmail.com>
2022-03-05 23:37:32 -0800

    learnmoresection updated

--- components/LearnMoreSection.js
+++ components/LearnMoreSection.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-const LearnMoreSection = ({id,title,content,imgSrcMobile,imgSrcDesktop}) =>{
+const LearnMoreSection = ({id,title,content,imgSrcMobile,imgSrcDesktop, linkUnderlineColor}) =>{
 
     const [screenSize,setScreenSize] = useState(null);
     useEffect(()=>{
@@ -12,17 +12,19 @@ const LearnMoreSection = ({id,title,content,imgSrcMobile,imgSrcDesktop}) =>{
 
     return (
         <LearnMoreSectionContainer sectionId={id}>
-            <LearnMoreImage src={ screenSize > 768 ? imgSrcDesktop : imgSrcMobile}>
-            </LearnMoreImage>
-            <LearnMore className={screenSize > 768 ? 'center':'center'}>
-                <div>
+            <LearnMoreImageContainer>
+                <LearnMoreImage src={ screenSize > 768 ? imgSrcDesktop : imgSrcMobile}>
+                </LearnMoreImage>
+            </LearnMoreImageContainer>
+            <LearnMoreContainer>
+                <LearnMore>
                     <LearnMoreHeader>{title}</LearnMoreHeader>
                     <LearnMoreContent>
-                        {content}
+                        <p>{content}</p>
                     </LearnMoreContent>
-                    <LearnMoreLink className={screenSize > 768 ? 'center':''}>LEARN MORE</LearnMoreLink>
-                </div>
-            </LearnMore>
+                    <LearnMoreLink underlineColor={linkUnderlineColor}><span>LEARN MORE</span></LearnMoreLink>
+                </LearnMore>
+            </LearnMoreContainer>
         </LearnMoreSectionContainer>
     );
 };
@@ -36,21 +38,25 @@ const LearnMoreSectionContainer = styled.div`
     @media (min-width: 768px){
         grid-template-columns: 50%;
         grid-template-areas:${props => props.sectionId == 1 ? `'LearnMore LearnMoreImage'` : `'LearnMoreImage LearnMore'`};
-        // grid-template-areas: "LearnMoreImage LearnMore";
     }
+`;
+const LearnMoreImageContainer = styled.div`
 `;
 const LearnMoreImage = styled.img`
-    width:100%;
+    width:100%; height:100%;
     grid-area:LearnMoreImage;
 `;
-const LearnMore = styled.div`
+const LearnMoreContainer = styled.div`
     padding:50px 25px 60px 25px;
     grid-area:LearnMore;
-    font-family: 'Fraunces', serif;
     @media (min-width: 768px){
-        padding:150px;
+        padding:19%;
         background-color:white;
     }
+`;
+const LearnMore = styled.div`
+    font-family: 'Fraunces', serif;
+    width:100%;
 `;
 const LearnMoreHeader = styled.h1`
 text-align:center;
@@ -65,6 +71,7 @@ font-family: 'Barlow', sans-serif;
 text-align:center;
 font-size:1.05em;
 font-weight:600;
+line-height: 1.8em;
 margin-bottom:30px;
 color: hsl(210, 4%, 67%);
 @media (min-width: 768px){
@@ -72,6 +79,26 @@ color: hsl(210, 4%, 67%);
 }
 `;
 const LearnMoreLink = styled.a`
+width:100%;
 font-weight:900;
+display:flex;
+justify-content:center;
+@media (min-width: 768px){
+    display:flex;
+    justify-content:flex-start;
+}
+& span{
+    display:inline-block;
+    &::after{
+        content: '';
+        height: 8px;
+        background:${props=>props.underlineColor};
+        border-radius: 5px;
+        display: block;
+        margin-top: -7px;
+        width: 110%;
+        margin-left: -5%;
+    }
+}
 `;
 export default LearnMoreSection;

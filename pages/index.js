c0d1ca0562ade68c6817c3084ca8e682eaa5249b
csmurillo <csmurillo00@gmail.com>
2022-03-08 19:21:55 -0800

    finished.

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,6 @@ import Head from 'next/head';
 export default () => (
   <>
     <Head>
-      <meta charset="UTF-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
       <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png"/>  
       <title>Frontend Mentor | Sunnyside agency landing page</title>
     </Head>

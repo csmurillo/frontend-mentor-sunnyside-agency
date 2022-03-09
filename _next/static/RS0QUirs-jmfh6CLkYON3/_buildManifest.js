596580ceb0c60825fdbe9b86e2d983e4b3dfcd46
csmurillo <csmurillo00@gmail.com>
2022-03-08 19:19:48 -0800

    Updates

--- _next/static/RS0QUirs-jmfh6CLkYON3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/379-d3a6415c54786858.js","static/chunks/pages/index-b6b3aab6e1fb92b9.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
